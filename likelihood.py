024a560411a44367ff335f44a3e58515d7a44d0e
Walter Cook <wcook8@ur.rochester.edu>
2024-06-08 23:21:16 -0400

    graph production

--- likelihood.py
+++ likelihood.py
@@ -7,8 +7,13 @@ from abc import ABC
 from astropy.io import fits
 
 from coprofile import COProfile
+from coprofile import COGenerator
+
+import matplotlib.pyplot as plt
 
 import pandas as pd
+
+from scipy import integrate
 
 class ModelLikelihood(ABC):
     """Model likelihood base class.
@@ -25,6 +30,25 @@ class ModelLikelihood(ABC):
             Profile depth averaging, in meters.
         rel_uncertainty: float
             Relative statistical uncertainty in measurements (0..1).
+            
+        Internal Values
+        ---------------
+        self.models : array ( axis0: parameter space ) of COProfiles
+            
+        self.CO_mods : array ( axis0: parameter space, axis1: depth )
+            
+        self.amplitudes : columns ( columns: 1, axis0: parameter space )
+            
+        self.fofactors : columns ( columns: 2, axis0: parameter space )
+            
+        self.parameters : array ( axis0: parameter space ; axis1: parameter type )
+            
+        self.volume : float
+            
+        self.dtheta : float
+            
+        self.logprior : array ( axis0: parameter space )
+            
         """
         hdus = fits.open(model_fits)
 
@@ -120,15 +144,8 @@ class ModelLikelihood(ABC):
             model, marginalizing over allowed model parameters.
         """
         N = len(CO_samp)
-        #Pr = 0.
-        #for model in self.models:
-            #loglike = -0.5*N*np.log(2*np.pi) - np.sum(0.5*((model.CO_model - CO_samp)/dCO_samp)**2 + np.log(dCO_samp))
-            #logprior = -np.log(self.volume)
-            #Pr += np.exp(loglike + logprior) * self.dtheta
-            
-        #loglike = -0.5*np.array([np.sum(((model.CO_model - CO_samp)/dCO_samp)**2)
-                                #for model in self.models]) - 0.5*N*np.log(2*np.pi) - np.sum(np.log(dCO_samp))
-            
+        
+        # axis0: parameter space; axis1: depth
         loglike = -0.5*np.sum(((self.CO_mods - CO_samp) / dCO_samp)**2, axis=1) - 0.5*N*np.log(2*np.pi) - np.sum(np.log(dCO_samp))
         #Pr = np.sum(np.exp(loglike + self.logprior) * self.dtheta)
         Pr = np.mean(np.exp(loglike + self.logprior)) #ASSUMING VOLUME = 1; dtheta = 1/N
@@ -175,115 +192,160 @@ class ModelLikelihood(ABC):
         # axis0 = iterations
         return Pr
     
-# I generalized ModelLikelihood, so we don't need these subclasses anymore
-'''class ConstModelLikelihood(ModelLikelihood):
-    """Model likelihood for GCR fluxes that are constant in time but have a systematic uncertainty in the cosmogenic 14CO production rates.
+    
+class FunctionModelLikelihood(ABC):
+    """Model likelihood built from functional dependence of f_mu_neg and f_mu_fast over time
     """
     
-    def __init__(self, model_fits, depth_avg=10, rel_uncertainty=0.02):
-        """Construct a model likelihood.
+    def __init__(self, generator, f_func, bounds, prior_func = None):
+        """Construct a model likelihood
         
         Parameters
         ----------
-        model_fits_file : str
-            Path to FITS file containing model parameters and 14CO profiles.
-        depth_avg: float
-            Profile depth averaging, in meters.
-        rel_uncertainty: float
-            Relative statistical uncertainty in measurements (0..1).
+        generator : COGenerator
+            
+        f_func : function
+            Functional dependence of f_mu_neg and f_mu_fast over time
+        
+            Parameters
+            ----------
+            t : array ( axis0: time )
+            
+            theta : tuple
+                
+            
+            Returns
+            -------
+            f_mu_neg : array ( axis0: time )
+                
+            f_mu_fast : array ( axis0: time )
+            
+        bounds : array ( axis0: params ) of tuples (min, max)
+            
+        prior_func : function
+            Bayesian prior on params
+            
+            Parameters
+            ----------
+            theta : tuple
+                
+                
+            Returns
+            -------
+            logprior : float
+                
+                
+        Internal Values
+        ---------------
+        
+            
         """
-        hdus = fits.open(model_fits)
-
-        # Set up 14CO profiles.
-        z = hdus['DEPTH'].data
-        co14 = hdus['CO14'].data
-        self.models = [COProfile(z, co, depth_avg, rel_uncertainty) for co in co14]
-        #self.models = []
-        
-        #for co in co14:
-            #cp = COProfile(z, co, depth_avg, rel_uncertainty)
-            #self.models.append(cp)
-            
-        # Input model parameters and set up uniform prior volume.
-        self.fofactors = hdus['FOFACTOR'].data
-        self.parameters = np.column_stack((self.fofactors['FOMUNEG'],self.fofactors['FOMUFAST']))
-        hdr = hdus['FOFACTOR'].header
-        
-        ncells = len(self.fofactors)
-        #volfrac = ncells / (hdr['N_FONEG']*hdr['N_FOFAST'])
-        #self.volume = volfrac * (hdr['FNEGMAX']-hdr['FNEGMIN']) * (hdr['FFASTMAX']-hdr['FFASTMIN'])
-        self.volume = 1 #If all we care about is calculating likelihood, we can always choose units so that volume = 1
-        
-        self.dtheta = self.volume / ncells
-        #self.logprior = np.array([-np.log(self.volume)
-                                  #for f in self.fofactors]) #log(prior) as a function of parameters
-        self.logprior = np.zeros(ncells) # log of uniform prior
-        self.logprior -= np.log(np.sum(np.exp(self.logprior) * self.dtheta)) # Normalize prior
-    
-
-class VariableModelLikelihood(ModelLikelihood):
-    """Model likelihood for GCR fluxes that vary in time.
-    """
-    
-    def __init__(self, model_fits, depth_avg=10, rel_uncertainty=0.02):
-        """Construct a model likelihood.
-        
-        Parameters
-        ----------
-        model_fits_file : str
-            Path to FITS file containing model parameters and 14CO profiles.
-        depth_avg: float
-            Profile depth averaging, in meters.
-        rel_uncertainty: float
-            Relative statistical uncertainty in measurements (0..1).
-        """
-        hdus = fits.open(model_fits)
-
-        # Set up 14CO profiles.
-        z = hdus['DEPTH'].data
-        co14 = hdus['CO14'].data
-        self.models = [COProfile(z, co, depth_avg, rel_uncertainty) for co in co14]
-            
-        # Input model parameters and set up uniform prior volume.
-        params = []
-        
-        if 'AMPL' in hdus: # Check for amplitudes
-            self.amplitudes = hdus['AMPL'].data
-            params.append(self.amplitudes['AMPL'])
-            
-            hdr = hdus['AMPL'].header
-            
-            n_amp = hdr['N_AMPL']
-            ampWidth = hdr['AMPLMAX'] - hdr['AMPLMIN'] if n_amp>1 else 1.
+        self.t = generator.t
+        self.generator = generator
+        self.f_func = f_func
+        self.bounds = bounds
+        if prior_func == None:
+            self.prior_func = self.unif_prior
         else:
-            n_amp = 1
-            ampWidth = 1.
-            
-        if 'FOFACTOR' in hdus: # Check for fofactors
-            self.fofactors = hdus['FOFACTOR'].data
-            params.append(self.fofactors['FOMUNEG'])
-            params.append(self.fofactors['FOMUFAST'])
-            
-            hdr = hdus['FOFACTOR'].header
-            
-            n_foneg = hdr['N_FONEG']
-            n_fofast = hdr['N_FOFAST']
-            negWidth = hdr['FNEGMAX'] - hdr['FNEGMIN'] if n_foneg>1 else 1.
-            fastWidth = hdr['FFASTMAX'] - hdr['FFASTMIN'] if n_fofast>1 else 1.
-        else:    
-            n_foneg = 1
-            n_fofast = 1
-            negWidth = 1.
-            fastWidth = 1.
+            self.prior_func = prior_func
+        self.norm = 0.
+        self.normalize()
         
-        self.parameters = np.array(params).T
+    def unif_prior(self, theta):
+        return 0.
+    
+    def logprior(self, theta):
+        return self.prior_func(theta) + self.norm
+    
+    def prior(self, *theta):
+        return np.exp(self.logprior(theta))
         
-        ncells = len(self.models)
+    def normalize(self):
+        self.norm = -np.log( integrate.nquad(self.prior, self.bounds)[0] )
+        return
         
-        #volfrac = ncells / (n_ampl * n_foneg * n_fofast) # fraction of a full cube taken up in parameter space
-        #self.volume = volfrac * ampWidth * negWidth * fastWidth # total volume in parameter space
-        self.volume = 1 #If all we care about is calculating likelihood, we can always choose units so that volume = 1
-            
-        self.dtheta = self.volume / ncells
-        self.logprior = np.zeros(ncells) # log of uniform prior
-        self.logprior -= np.log(np.sum(np.exp(self.logprior) * self.dtheta)) # Normalize prior'''
+    def CO(self, theta):
+        """
+        Parameters
+        ----------
+        z_samp : array ( axis0 : depth )
+        
+        theta : tuple
+        
+        Returns
+        -------
+        CO : array ( axis0 : depth )
+        """
+        return self.generator.generate(*self.f_func(self.t, theta)).CO_model
+        
+    def prob(self, *theta):
+        N = len(self.CO_samp)
+        
+        loglike = -0.5*np.sum(((self.CO(theta) - self.CO_samp) / self.dCO_samp)**2) - 0.5*N*np.log(2*np.pi) - np.sum(np.log(self.dCO_samp))
+        
+        return np.exp(loglike + self.logprior(theta))
+        
+    def likelihood(self, z_samp, CO_samp, dCO_samp):
+        """Compute likelihood of a given realized 14CO profile given this model.
+
+        Parameters
+        ----------
+        z_samp : list or ndarray
+            Sampled depths of a realized 14CO profile.
+        CO_samp: list or ndarray
+            Realization of a 14CO profile data set.
+        dCO_samp: list or ndarray
+            Statistical uncertainties in realization of a 14CO profile.
+
+        Returns
+        -------
+        Pr : float
+            Likelihood of observing a given realized 14CO profile given this
+            model, marginalizing over allowed model parameters.
+        """
+        # this is a really janky hack, but it should work
+        self.z_samp = z_samp
+        self.CO_samp = CO_samp
+        self.dCO_samp = dCO_samp
+        return integrate.nquad(self.prob, self.bounds)
+    
+    def plot_factors(self, thetas):
+        neg = np.zeros((len(thetas),len(self.t)))
+        fast = np.zeros((len(thetas),len(self.t)))
+        for i,theta in enumerate(thetas):
+            n, f = self.f_func(self.t,theta)
+            neg[i] = n
+            fast[i] = f
+
+        fig, ax = plt.subplots(1,1, figsize=(6,5), tight_layout=True)
+        for i,n in enumerate(neg):
+            ax.plot(self.t, n, label='{}'.format(thetas[i]))
+        ax.set(xlim=(min(self.t),max(self.t)),
+               xlabel='time [yrs]',
+                ylim=(0.02,0.12),
+                ylabel='f_mu_neg')
+        ax.legend(loc='lower right', fontsize=10)
+        plt.title('f_mu_neg over time')
+
+        fig, ax = plt.subplots(1,1, figsize=(6,5), tight_layout=True)
+        for i,f in enumerate(fast):
+            ax.plot(self.t, f, label='{}'.format(thetas[i]))
+        ax.set(xlim=(min(self.t),max(self.t)),
+                xlabel='time [yrs]',
+                ylim=(0.02,0.12),
+                ylabel='f_mu_fast')
+        ax.legend(loc='lower right', fontsize=10)
+        plt.title('f_mu_fast over time')
+        
+    def plot_profiles(self, thetas):
+        p = [self.generator.generate(*self.f_func(self.t, theta)) for theta in thetas]
+
+        fig, ax = plt.subplots(1,1, figsize=(6,5), tight_layout=True)
+        for i,profile in enumerate(p):
+            ax.plot(profile.z, profile.CO, label='{}'.format(thetas[i]))
+        ax.set(xlim=(90,300),
+                xlabel='depth [m]',
+                ylim=(0,30),
+                ylabel=r'$^{14}$CO concentration [molecule g$^{-1}$]')
+        ax.legend(loc='lower right', fontsize=10)
+        plt.title('CO Profiles')
--- null-generator.py
+++ null-generator.py
@@ -27,7 +27,7 @@ from argparse import ArgumentParser
 
 import random
 
-fmu_neg, fmu_fast = 0.066, 0.072
+fmu_neg, fmu_fast = 0.068, 0.076
 
 if __name__ == '__main__':
     p = ArgumentParser(description='BF null distribition generator')
@@ -35,25 +35,47 @@ if __name__ == '__main__':
                    help='Depth average of CO profile, in m.')
     p.add_argument('-u', '--reluncertainty', type=float, default=0.02,
                    help='Depth average of CO profile, in m.')
-    p.add_argument('-n', '--number', type=int, default=10000,
+    p.add_argument('-n', '--number', type=int, default=5000000,
+                   help='Number of trials to generate')
+    p.add_argument('-f', '--fixed', type=bool, default=False,
+                   help='Number of trials to generate')
+    p.add_argument('-s', '--sample', type=bool, default=False,
+                   help='Number of trials to generate')
+    p.add_argument('-p', '--priors', type=bool, default=False,
+                   help='Number of trials to generate')
+    p.add_argument('-fo', '--fofactors', type=bool, default=False,
+                   help='Number of trials to generate')
+    p.add_argument('-nl', '--null', type=bool, default=False,
                    help='Number of trials to generate')
     p.add_argument('-i', '--id', type=int, default=1,
                    help='Simulation number')
     args = p.parse_args()
     
+    print(args)
+
+    fixed = 'pres' if args.fixed else 'past'
+    sample = 'mult' if args.sample else 'one'
+    priors = 'non-unif' if args.priors else 'unif'
+    f_factors = 'all' if args.fofactors else 'const'
+    null = 'null-out' if args.null else 'null-in'
+    print(fixed, sample, priors, f_factors, null)
+    
     print('Loading model data...')
 
-    const_models = ModelLikelihood('models/balco_14co_const_models.fits', depth_avg=args.depthavg)
-    linear_models = ModelLikelihood('models/balco_14co_linear_models_SMALL_FIX.fits', depth_avg=args.depthavg)
-    step_models = ModelLikelihood('models/balco_14co_step_models_SMALL_FIX.fits', depth_avg=args.depthavg)
-    burst_models = ModelLikelihood('models/balco_14co_burst_models.fits', depth_avg=args.depthavg)
+    const_models = ModelLikelihood('FITS_models/balco_14co_const_models.fits', depth_avg=args.depthavg)
+    linear_models = ModelLikelihood('FITS_models/balco_14co_linear_models_{}_{}_{}.fits'.format(fixed, f_factors, null), 
+                                    depth_avg=args.depthavg)
+    step_models = ModelLikelihood('FITS_models/balco_14co_step_models_{}_{}_{}.fits'.format(fixed, f_factors, null), 
+                                  depth_avg=args.depthavg)
+    burst_models = ModelLikelihood('FITS_models/balco_14co_burst_models_{}.fits'.format(f_factors), depth_avg=args.depthavg)
     
-    print('Generating priors...')
-    
-    const_models.import_prior('14CO_f_all_ChiSq.csv')
-    #linear_models.import_prior('14CO_f_all_ChiSq.csv')
-    #step_models.import_prior('14CO_f_all_ChiSq.csv')
-    #burst_models.import_prior('14CO_f_all_ChiSq.csv')
+    if args.priors:
+        print('Generating priors...')
+
+        const_models.import_prior('14CO_f_all_ChiSq.csv')
+        linear_models.import_prior('14CO_f_all_ChiSq.csv')
+        step_models.import_prior('14CO_f_all_ChiSq.csv')
+        burst_models.import_prior('14CO_f_all_ChiSq.csv')
     
     print('Calculating Bayes factors')
     
@@ -61,25 +83,42 @@ if __name__ == '__main__':
     j = dist.argmin()
     data = const_models.models[j]
     
-    #data_mult = random.choices(const_models.models, weights=np.exp(const_models.logprior),k=args.number)
+    data_mult = random.choices(const_models.models, weights=np.exp(const_models.logprior),k=args.number)
 
     BF_lin_null = np.zeros(args.number)
     BF_step_null = np.zeros(args.number)
     BF_100yr_null = np.zeros(args.number)
-
-    for i in tqdm(range(args.number)):
-        z_samp, CO_samp, dCO_samp = data.sample_z()
-        #z_samp, CO_samp, dCO_samp = data_mult[i].sample_z()
+    
+    if args.sample:
+        data_mult = random.choices(const_models.models, weights=np.exp(const_models.logprior),k=args.number)
         
-        const_like = const_models.likelihood(z_samp, CO_samp, dCO_samp)
+        for i in tqdm(range(args.number)):
+            z_samp, CO_samp, dCO_samp = data_mult[i].sample_z()
 
-        BF_lin_null[i] = const_like / linear_models.likelihood(z_samp, CO_samp, dCO_samp)
+            const_like = const_models.likelihood(z_samp, CO_samp, dCO_samp)
 
-        BF_step_null[i] = const_like / step_models.likelihood(z_samp, CO_samp, dCO_samp)
+            BF_lin_null[i] = const_like / linear_models.likelihood(z_samp, CO_samp, dCO_samp)
 
-        BF_100yr_null[i] = const_like / burst_models.likelihood(z_samp, CO_samp, dCO_samp)
+            BF_step_null[i] = const_like / step_models.likelihood(z_samp, CO_samp, dCO_samp)
 
-    np.savez(f'models/bf_null_{args.depthavg:g}m_{100*args.reluncertainty:g}pct_{args.number:06d}_{args.id:02d}.npz',
+            BF_100yr_null[i] = const_like / burst_models.likelihood(z_samp, CO_samp, dCO_samp)
+            
+    else:
+        dist = (const_models.fofactors['FOMUNEG'] - fmu_neg)**2 + (const_models.fofactors['FOMUFAST'] - fmu_fast)**2
+        j = dist.argmin()
+        data = const_models.models[j]
+
+        for i in tqdm(range(args.number)):
+            z_samp, CO_samp, dCO_samp = data.sample_z()
+
+            const_like = const_models.likelihood(z_samp, CO_samp, dCO_samp)
+
+            BF_lin_null[i] = const_like / linear_models.likelihood(z_samp, CO_samp, dCO_samp)
+
+            BF_step_null[i] = const_like / step_models.likelihood(z_samp, CO_samp, dCO_samp)
+
+            BF_100yr_null[i] = const_like / burst_models.likelihood(z_samp, CO_samp, dCO_samp)
+    np.savez(f'FITS_models/bf_null_{args.depthavg:g}m_{100*args.reluncertainty:g}pct_{args.number:06d}_{fixed}_{sample}_{priors}_{f_factors}_{null}_{args.id:02d}.npz',
              BF_lin=BF_lin_null,
              BF_step=BF_step_null,
              BF_100yr=BF_100yr_null)
